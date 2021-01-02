0a32090d607a8ab6e7702abe898ad351d083cde2
Andrew Sue <ansue1234@gmail.com>
2021-01-02 01:05:25 -0800

    fixed small issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { css } from "@emotion/react"
-import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default function Home({ data }) {
   console.log(data)
   return (
     <Layout>
-      <h1>Hello</h1>
+      <h1>Hello! Welcome to the personal site of Andrew Sue!</h1>
       <p>This site is under construction, please check back later!</p>
     </Layout>
   )
